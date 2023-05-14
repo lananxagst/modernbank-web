0bcdf009874affd1ee4f2081fd0e4c34fced7bb4
lananxagst <67673253+lanang210801@users.noreply.github.com>
2023-05-15 01:03:18 +0800

    add image on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { discount } from "../assets";
+import { discount, robot } from "../assets";
 import styles from "../style";
 import GetStarted from "./GetStarted";
 
@@ -23,12 +23,29 @@ const Hero = () => {
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52] text-white ss:leading-[100px] leading-[75px]">
             The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span> {""}
-            Payment Method Baru Gati
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
         </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+          Payment Method.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
+      </div>
+
+      <div>
+        <img
+          src={robot}
+          alt="billing"
+          className="w-[100%] h-[100px] relative z-[5]"
+        />
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
       </div>
     </section>
   );
