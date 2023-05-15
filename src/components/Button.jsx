a8b0516064666d4e91ea074d37d02ae765bc228f
lananxagst <67673253+lanang210801@users.noreply.github.com>
2023-05-15 11:59:04 +0800

    create content in hero and businnes section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 
-const Button = () => {
+// eslint-disable-next-line react/prop-types
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
